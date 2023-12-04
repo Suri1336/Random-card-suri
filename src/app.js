fa9b825763b3539ad292af47abc048a19391fcc5
Suri1336 <146222294+Suri1336@users.noreply.github.com>
2023-12-04 19:45:00 +0000

    done.js

--- src/app.js
+++ src/app.js
@@ -9,6 +9,25 @@ window.onload = function() {
   //write your code here
 
   let randomSuits = ["♦", "♥", "♠", "♣"];
-  let randomNumber = Math.floor(Math.random() * 4);
-  document.querySelector(".top-suit").innerHTML = randomSuits[randomNumber];
+  let randomSuitNumber = Math.floor(Math.random() * randomSuits.length);
+  document.querySelector(".top-suit").innerHTML = randomSuits[randomSuitNumber];
+  document.querySelector(".bottom-suit").innerHTML =
+    randomSuits[randomSuitNumber];
+
+  let randomCardNumbers = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "J",
+    "Q",
+    "K"
+  ];
+  let randomNumber = Math.floor(Math.random() * randomCardNumbers.length);
+  document.querySelector(".number").innerHTML = randomCardNumbers[randomNumber];
 };
