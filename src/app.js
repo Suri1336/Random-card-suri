e33125af923fffed094fd5962d87e7186a3604bb
Suri1336 <146222294+Suri1336@users.noreply.github.com>
2023-12-08 21:53:52 +0000

    finished no button

--- src/app.js
+++ src/app.js
@@ -5,36 +5,43 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+let SuitsArray = ["♦", "♥", "♠", "♣"];
+let numbersArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+let deck = [];
+
+window.onload =
   //write your code here
 
-  let SuitsArrey = ["♦", "♥", "♠", "♣"];
-  let numbersArrey = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "J",
-    "Q",
-    "K"
-  ];
+  function() {
+    for (let suit of SuitsArray) {
+      for (let number of numbersArray) {
+        deck.push({ suit, number });
+      }
+    }
+    deck = shuffleDeck(deck);
+    displayCard(deck[0]);
+  };
+function shuffleDeck(deck) {
+  for (let decksize = deck.length - 1; decksize > 0; decksize--) {
+    const j = Math.floor(Math.random() * (decksize + 1));
+    [deck[decksize], deck[j]] = [deck[j], deck[decksize]];
+  }
+  return deck;
+}
 
-  
-  let randomSuitIndex = Math.floor(Math.random() * SuitsArrey.length);
-  let randomCardNumberIndex = Math.floor(Math.random() * numbersArrey.length);
+let randomSuitIndex = Math.floor(Math.random() * SuitsArray.length);
+let randomCardNumberIndex = Math.floor(Math.random() * numbersArray.length);
 
-  let randomSuit = SuitsArrey[randomSuitIndex];
-  let randomNumber = numbersArrey[randomCardNumberIndex];
-
+let randomSuit = SuitsArray[randomSuitIndex];
+let randomNumber = numbersArray[randomCardNumberIndex];
+function displayCard(card) {
   let topSuitDiv = document.querySelector(".top-suit");
   let bottomSuitDiv = document.querySelector(".bottom-suit");
   let numberDiv = document.querySelector(".number");
-
+  // let button = document.querySelector(".btn");
+  // button.addEventListener("click", function() {
+  //   Generatecard();
+  // });
   bottomSuitDiv.innerHTML = randomSuit;
   topSuitDiv.innerHTML = randomSuit;
   numberDiv.innerHTML = randomNumber;
@@ -42,5 +49,9 @@ window.onload = function() {
     topSuitDiv.style.color = "red";
     bottomSuitDiv.style.color = "red";
     numberDiv.style.color = "red";
+  } else {
+    topSuitDiv.style.color = "black";
+    bottomSuitDiv.style.color = "black";
+    numberDiv.style.color = "black";
   }
-};
+}
